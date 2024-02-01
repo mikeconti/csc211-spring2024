93744158298435f87b834127904030ec3e2d9dcf
Michael Conti <michaelconti@uri.edu>
2024-02-01 13:07:58 -0500

    code

---
+++ code/addition.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+
+    int a, b;
+
+    std::cin >> a;
+    std::cin >> b;
+
+    int c = a + b;
+
+    std::cout << a << " + " << b << " = " << c;
+
+
+
+    
+
+    return 0;
+}
